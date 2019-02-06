5acdd09474e96afce340393ea53d1a7fed744409
Eric Puskas <eric.puskas@yahoo.com>
2019-02-07 00:30:49 +0200

    Fix Build

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use("/api/messages", messages);
 // Serve static assets if in production
 if (NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("../client/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
