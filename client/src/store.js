146dbb307832695721f8ca2997b28e69dc281c21
Eric Puskas <eric.puskas@yahoo.com>
2019-02-08 03:12:08 +0200

    Remove Redux Dev Tools

--- client/src/store.js
+++ client/src/store.js
@@ -9,10 +9,9 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    process.env.NODE_ENV === "development" &&
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true, traceLimit: 25 })
+    applyMiddleware(...middleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    // window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true, traceLimit: 25 })
   )
 );
 
